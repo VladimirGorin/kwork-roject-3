a295dd66ffff3319163c0a38230c61a7e33fce36
Владимир Горин <vladimirgorin410@gmail.com>
2022-04-19 02:55:26 +0300

    first commit

---
+++ accets/js/main.js
@@ -0,0 +1,136 @@
+
+function openPopup_1(){
+    document.getElementById('popup_1').style.display = 'flex'
+
+}
+
+function openPopup_2(){
+    document.getElementById('popup_2').style.display = 'flex'
+
+}
+
+function openPopup_3(){
+    document.getElementById('popup_3').style.display = 'flex'
+
+}
+
+function openPopup_4(){
+    document.getElementById('popup_4').style.display = 'flex'
+
+}
+
+function openPopup_5(){
+    document.getElementById('popup_5').style.display = 'flex'
+
+}
+
+
+
+function save_1(){
+    document.getElementById('popup_1').style.display = 'none'
+
+}
+
+function save_2(){
+    document.getElementById('popup_2').style.display = 'none'
+
+}
+
+
+function save_3(){
+    document.getElementById('popup_3').style.display = 'none'
+
+}
+
+
+function save_4(){
+    document.getElementById('popup_4').style.display = 'none'
+
+}
+
+
+function save_5(){
+    document.getElementById('popup_5').style.display = 'none'
+
+}
+
+
+
+
+
+function closeMenu(){
+    document.getElementById('menu-1').style.display = 'none'
+
+}
+function closeMenu_2(){
+    document.getElementById('menu-2').style.display = 'none'
+
+}
+
+function closeMenu_3(){
+    document.getElementById('menu-3').style.display = 'none'
+
+}
+
+function closeMenu_4(){
+    document.getElementById('menu-4').style.display = 'none'
+
+}
+
+function closeMenu_5(){
+    document.getElementById('menu-5').style.display = 'none'
+
+}
+
+function closeMenu_6(){
+    document.getElementById('menu-6').style.display = 'none'
+
+}
+
+function closeMenu_7(){
+    document.getElementById('menu-7').style.display = 'none'
+
+}
+
+
+function openMenu(){
+    document.getElementById('menu-1').style.display = 'flex'
+
+}
+
+function openMenu_2(){
+    document.getElementById('menu-2').style.display = 'flex'
+
+}
+
+function openMenu_3(){
+    document.getElementById('menu-3').style.display = 'flex'
+
+}
+
+function openMenu_4(){
+    document.getElementById('menu-4').style.display = 'flex'
+
+}
+
+function openMenu_5(){
+    document.getElementById('menu-5').style.display = 'flex'
+
+}
+
+function openMenu_6(){
+    document.getElementById('menu-6').style.display = 'flex'
+
+}
+
+function openMenu_7(){
+    document.getElementById('menu-7').style.display = 'flex'
+
+}
+
+function pasteText(){
+    var value = document.getElementById('exampleInputSendMessage1').value
+    var paste = document.getElementById('textDS')
+
+    paste.innerText =  `${value}`
+}
